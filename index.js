2ed8a0209c5d971259997da634f2cd25f30df76a
Dmitriy Yurkov <buzia3lic@gmail.com>
2024-03-05 18:49:46 +0300

    js mod/add menu tabs/add info

--- index.js
+++ index.js
@@ -1,46 +1,63 @@
 function showMenu() {
     const x = document.getElementById("mainMenu");
-    // const y = document.querySelector(".grid_tickets");
     const y = document.getElementById("main");
+    console.log(y);
     if (x.style.width === "0px") {
-        x.style.width = "80px";
-        y.style.marginLeft = "0px";
+        x.style.width = "60px";
+        y.style.marginLeft = "60px";
     } else {
         x.style.width = "0px";
-        y.style.marginLeft = "-80px";
+        y.style.marginLeft = "0px";
     }
-    // const x = document.getElementById("mainMenu");
-    // if (x.style.display === "flex") {
-    //     x.style.display = "none";
-    // } else {
-    //     x.style.display = "flex";
-    // }
 }
+
+
 
 const mainMenuItemArr = document.querySelectorAll(".grid_menu_items");
-
-function mainMuneItemActive(event) {
-    for (mainMuneItem of mainMenuItemArr) {
-        mainMuneItem.classList.remove('grid_menu_items_active');
-    }
-    event.currentTarget.classList.add('grid_menu_items_active');
-}
-
 const ticketMenuItemArr = document.querySelectorAll(".ticket_header_menu_item");
-
-function ticketMuneItemActive(event) {
-    for (ticketMuneItem of ticketMenuItemArr) {
-        ticketMuneItem.classList.remove('ticket_header_menu_item_active');
-    }
-    event.currentTarget.classList.add('ticket_header_menu_item_active');
-}
-
-
 const ticketListArr = document.querySelectorAll('.ticket_list_item');
+const mainContentArr = document.querySelectorAll('.main-content')
 
-function ticketListItemActiv(event) {
-    for (ticketListItem of ticketListArr) {
-        ticketListItem.classList.remove('ticket_list_item_active');
-    }
-    event.currentTarget.classList.add('ticket_list_item_active');
+
+// Переключение табов
+mainMenuItemArr.forEach((tab, index) => {
+    tab.onclick = () => {
+        changeTab(index)
+    };
+
+});
+
+function changeTab(index) {
+    setActiveaClass(mainMenuItemArr, index);
+    setActiveaClass(mainContentArr, index);
 }
+
+function setActiveaClass(arr, index) {
+    for (let el of arr) {
+        el.classList.remove('active');
+    }
+    arr[index].classList.add('active');
+}
+
+
+
+
+//  Переключатель состояния актив.
+ activeSwitcher(ticketMenuItemArr);
+ activeSwitcher(ticketListArr);
+
+function delActive(arr) {
+    for (let arrItem of arr) {
+        arrItem.classList.remove('active');
+    }
+}
+
+function activeSwitcher(arr) {
+    for (let arrItem of arr) {
+        arrItem.onclick = function() {
+            delActive(arr);
+            this.classList.add('active');
+        }
+    }
+}
+
